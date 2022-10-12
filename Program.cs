e138d11456dec7b1e0406fe2aaa45372dead63bc
oleg <ojiegoff@gmail.com>
2022-10-12 15:24:04 +0500

    Этап2 - вводим цикл для переборки и сравнения длины символов в масиве

--- Program.cs
+++ Program.cs
@@ -3,5 +3,24 @@
 // При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
 
 string[] array1 = new string[5] {"hello", "2", "world", ":-)", "yes"};
+PrintArray(array1);
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
